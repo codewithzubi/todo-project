e6d83d2b714ca81921ae60f109aef2b70059a70f
codewithzubi <za8089006@gmail.com>
2024-04-01 04:40:36 -0700

    todo-project

---
+++ index.js
@@ -0,0 +1,64 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let tasks = [];
+const commandMenu = async () => {
+    let choice = await inquirer.prompt([
+        {
+            name: "menu",
+            type: "list",
+            message: "Command Menu",
+            choices: ["Add Task", "Display Task", "Delete Task", "Exit Task"],
+        },
+    ]);
+    if (choice.menu === "Add Task") {
+        let task = await inquirer.prompt([
+            {
+                name: "task",
+                type: "input",
+                message: "Enter the Task",
+            },
+        ]);
+        tasks.push(task.task);
+        console.log("Task added successfully!");
+        commandMenu();
+    }
+    else if (choice.menu === "Display Task") {
+        if (tasks.length === 0) {
+            console.log("NO TASK TO DISPLAY");
+        }
+        else {
+            console.log("TASKS:");
+            tasks.forEach((task, index) => {
+                console.log(`${index + 1}. ${task} `);
+            });
+        }
+        commandMenu();
+    }
+    else if (choice.menu === "Delete Task") {
+        if (tasks.length === 0) {
+            console.log("NO TASK TO DELETE");
+            commandMenu();
+            return;
+        }
+        let indexNumber = await inquirer.prompt([
+            {
+                name: "index",
+                type: "input",
+                message: "Enter the task number to delete:",
+            },
+        ]);
+        let taskIndexNumber = parseInt(indexNumber.index);
+        if (taskIndexNumber >= 1 && taskIndexNumber <= tasks.length) {
+            const deletedTask = tasks.splice(taskIndexNumber - 1, 1)[0];
+            console.log(`Task '${deletedTask}' deleted successfully!`);
+        }
+        else {
+            console.log("INVAlID TASK INDEX");
+        }
+        commandMenu();
+    }
+    else {
+        console.log("EXITING....");
+    }
+};
+commandMenu();
